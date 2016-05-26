d53c5670219fe98fbdd4a08e1a4b4c77de2621f1
Rinat Khabibiev <srenskiy@gmail.com>
2016-05-26 08:22:39 +0300

    new version: 0.1.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as description:
 
 setup(
     name='fabricio',
-    version='0.1.5',
+    version='0.1.6',
     author='Rinat Khabibiev',
     author_email='srenskiy@gmail.com',
     packages=list(map('fabricio.'.__add__, find_packages('fabricio'))) + ['fabricio'],
