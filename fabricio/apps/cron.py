75a74a83c001dda86aa75e492e741bda2267f2cb
Rinat Khabibiev <srenskiy@gmail.com>
2016-07-09 13:54:07 +0300

    CronContainer is deprecated now and will be removed in v0.3

--- fabricio/apps/cron.py
+++ fabricio/apps/cron.py
@@ -1,3 +1,5 @@
+import warnings
+
 from fabricio import docker
 
 
@@ -7,6 +9,17 @@ class CronContainer(docker.Container):
     from images which inherit for example this image:
     https://hub.docker.com/r/renskiy/cron/
     """
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            'CronContainer is deprecated and will be removed in v0.3',
+            DeprecationWarning,
+        )
+        warnings.warn(
+            'CronContainer will be removed in Fabricio v0.3',
+            RuntimeWarning,
+        )
+        super(CronContainer, self).__init__(*args, **kwargs)
 
     cmd = '/bin/bash -c "cron && tail -f /var/log/cron.log"'
 
