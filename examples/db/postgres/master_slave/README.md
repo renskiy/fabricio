c7a7b86548c0e324f638263fd3e1acfd19be9669
Rinat Khabibiev <srenskiy@gmail.com>
2016-11-16 01:11:00 +0300

    use Virtualbox private network instead of public

--- examples/db/postgres/master_slave/README.md
+++ examples/db/postgres/master_slave/README.md
@@ -3,9 +3,11 @@
 This configuration based on PostgreSQL [streaming replication](https://wiki.postgresql.org/wiki/Streaming_Replication).
 
 ## Requirements
-* Fabricio 0.3.8 or greater
+* Fabricio 0.3.12 or greater
 * [Vagrant](https://www.vagrantup.com)
 * One from the [list of Vagrant supported providers](https://www.vagrantup.com/docs/providers/) (this example was tested with [VirtualBox](https://www.virtualbox.org/))
+* [Docker](https://www.docker.com/products/overview) for Linux/Mac/Windows
+* Docker registry which runs locally on 5000 port, this can be reached out by executing following docker command: `docker run --name registry --publish 5000:5000 --detach registry:2`
 
 ## Files
 * __fabfile.py__, Fabricio configuration
@@ -44,6 +46,5 @@ Add new VM definition to `Vagrantfile` and then run deploy again:
 
 ## Issues
 
-* If your host machine has more then one network adapter `Vagrant` will [ask](https://www.vagrantup.com/docs/networking/public_network.html#default-network-interface) you which one will be used
-* Sometimes `VirtualBox` can't assign proper IP address to VM's bridged network adapter. In such cases destroying VM and creation new one will help (`vagrant reload` usually does not help)
 * If you see warnings in `Vagrant` logs about Guest Extensions version is not match VirtualBox version try to install `vagrant-vbguest` plugin that automatically installs Guest Extensions of version which corresponds to your version of VirtualBox: `vagrant plugin install vagrant-vbguest`
+* Docker for Mac quite often begins restarting during image pull process. This may lead to complete inability to finish deploy process. Docker's "Reset to factory defaults" usually helps in this situation.
--- examples/db/postgres/master_slave/fabfile.py
+++ examples/db/postgres/master_slave/fabfile.py
@@ -1,55 +1,6 @@
-import re
-
-from cached_property import cached_property
-from fabric import api as fab
 from fabricio import tasks
 from fabricio.apps.db.postgres import StreamingReplicatedPostgresqlContainer
-
-
-class AvailableVagrantHosts(object):
-
-    def __iter__(self):
-        return iter(self.hosts)
-
-    @cached_property
-    def hosts(self):
-        keys = fab.env.key_filename = []
-        hosts = []
-        fab.local('vagrant up')
-        ssh_configs_data = fab.local('vagrant ssh-config', capture=True)
-        ssh_configs = map(
-            lambda config: dict(map(
-                lambda row: row.lstrip().split(' ', 1),
-                config.splitlines()
-            )),
-            re.split('(?m)\s*^$\s*', ssh_configs_data),
-        )
-        for ssh_config in ssh_configs:
-            keys.append(ssh_config['IdentityFile'])
-            host_string = '{User}@{HostName}:{Port}'.format(**ssh_config)
-            with fab.settings(
-                host_string=host_string,
-                # see https://github.com/fabric/fabric/issues/1522
-                # disable_known_hosts=True,
-            ):
-                ip_command = (
-                    "ip addr show eth1 "
-                    "| grep inet "
-                    "| head -1 "
-                    "| awk '{ print $2 }'"
-                )
-                ip = ssh_config['Host'] = fab.run(
-                    ip_command,
-                    quiet=True,
-                ).split('/')[0]
-                if not ip:
-                    error_msg = 'Could not find IP address of ' + host_string
-                    raise ValueError(error_msg)
-            host = '{User}@{Host}'.format(**ssh_config)
-            fab.puts('Added host: ' + host)
-            hosts.append(host)
-        return hosts
-
+from fabricio.misc import AvailableVagrantHosts
 
 db = tasks.DockerTasks(
     container=StreamingReplicatedPostgresqlContainer(
@@ -68,5 +19,7 @@ db = tasks.DockerTasks(
             ports='5432:5432',
         ),
     ),
-    hosts=AvailableVagrantHosts(),
+    hosts=AvailableVagrantHosts(guest_network_interface='eth1'),
+    registry='localhost:5000',
+    ssh_tunnel_port=5000,
 )
