b4e0294eed4dce7d5170cbc41e0bbec5e6387e82
Rinat Khabibiev <srenskiy@gmail.com>
2017-03-06 20:57:47 +0300

    Revert "improved config"
    This reverts commit 09b58a883f0e6a3adddc59d82c5610c7c1232792.

--- examples/service/swarm/fabfile.py
+++ examples/service/swarm/fabfile.py
@@ -16,7 +16,7 @@ def swarm_init():
     def _swarm_init():
         if swarm_init.worker_join_command is None:
             fabricio.run(
-                'docker swarm init',
+                'docker swarm init --advertise-addr {0}'.format(fab.env.host),
                 ignore_errors=True,
             )
             join_token = fabricio.run(
