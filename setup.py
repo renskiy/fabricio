b41f0ffbbe5a8f126390927da3779a8d73b5042b
Rinat Khabibiev <srenskiy@gmail.com>
2017-12-05 23:22:39 +0300

    new version: 0.4.5

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if sys.version_info < (2,7):
 
 setup(
     name='fabricio',
-    version='0.4.4',
+    version='0.4.5',
     author='Rinat Khabibiev',
     author_email='srenskiy@gmail.com',
     packages=list(map('fabricio.'.__add__, find_packages('fabricio'))) + ['fabricio'],
