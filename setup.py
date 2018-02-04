b75417ac4a848a71047595314380500660e896fa
Rinat Khabibiev <srenskiy@gmail.com>
2018-02-04 16:21:55 +0300

    new version: 0.5.6

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 
 setup(
     name='fabricio',
-    version='0.5.5',
+    version='0.5.6',
     author='Rinat Khabibiev',
     author_email='srenskiy@gmail.com',
     packages=list(map('fabricio.'.__add__, find_packages('fabricio'))) + ['fabricio'],
